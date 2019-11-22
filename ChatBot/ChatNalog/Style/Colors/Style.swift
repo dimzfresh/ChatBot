ce7182c4574b4b9c487753f38ee009c531076dde
Dmitrii Ziablikov <dimzfresh@gmail.com>
2019-11-22 17:04:22 +0300

    first commit

---
+++ ChatBot/ChatNalog/Style/Colors/Style.swift
@@ -0,0 +1,15 @@
+//
+//  Style.swift
+//  ChatBot
+//
+//  Created by Dmitrii Ziablikov on 20/11/2019.
+//  Copyright © 2019 di. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var brandColor: UIColor {
+        return #colorLiteral(red: 0.4549019608, green: 0.6392156863, blue: 0.7803921569, alpha: 1)
+    }
+}
---
+++ ChatBot/ChatNalog/UI/MainScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatBot
+//
+//  Created by Dmitrii Ziablikov on 20/11/2019.
+//  Copyright © 2019 di. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
